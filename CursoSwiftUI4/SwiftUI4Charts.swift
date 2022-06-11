8a45d7c570a823859fd317d0322305ccba339a86
Juan Gabriel Gomila <juangabriel@frogames.es>
2022-06-11 11:22:35 +0200

    Hemos creado un primer fichero de Swift UI

---
+++ CursoSwiftUI4/SwiftUI4Charts.swift
@@ -0,0 +1,20 @@
+//
+//  SwiftUI4Charts.swift
+//  CursoSwiftUI4
+//
+//  Created by Juan Gabriel Gomila Salas on 11/6/22.
+//
+
+import SwiftUI
+
+struct SwiftUI4Charts: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SwiftUI4Charts_Previews: PreviewProvider {
+    static var previews: some View {
+        SwiftUI4Charts()
+    }
+}
