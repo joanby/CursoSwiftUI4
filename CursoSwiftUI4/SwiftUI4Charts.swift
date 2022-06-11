8bab7cd73253df81c44e592c61f5d0332a7f148f
Juan Gabriel Gomila <joan-by@hotmail.com>
2022-06-11 13:26:54 +0200

    Fin de la actualización sobre Charts

--- CursoSwiftUI4/SwiftUI4Charts.swift
+++ CursoSwiftUI4/SwiftUI4Charts.swift
@@ -6,10 +6,34 @@
 //
 
 import SwiftUI
+import Charts
 
 struct SwiftUI4Charts: View {
+    
+    let weekdays = Calendar.current.shortWeekdaySymbols
+    let steps = [4809, 11463, 5685, 6774, 5058, 6990, 4079]
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Chart{
+            ForEach(weekdays.indices, id: \.self){ idx in
+                BarMark(x: .value("Día", weekdays[idx]) ,
+                        y: .value("Pasos", steps[idx])
+                )
+                .foregroundStyle(by: .value("Día", weekdays[idx]))
+                .annotation{
+                    Text("\(steps[idx])")
+                }
+                
+                PointMark(x: .value("Día", weekdays[idx]) ,
+                         y: .value("Pasos", steps[idx]))
+                    .foregroundStyle(.black)
+                    .lineStyle(StrokeStyle(lineWidth: 5))
+            }
+            
+            RuleMark(y: .value("Objetivo Diario", 10000))
+                .foregroundStyle(.red)
+            
+        }
     }
 }
 
---
+++ CursoSwiftUI4/SwiftUI4ResizableBS.swift
@@ -0,0 +1,20 @@
+//
+//  SwiftUI4ResizableBS.swift
+//  CursoSwiftUI4
+//
+//  Created by Juan Gabriel Gomila Salas on 11/6/22.
+//
+
+import SwiftUI
+
+struct SwiftUI4ResizableBS: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SwiftUI4ResizableBS_Previews: PreviewProvider {
+    static var previews: some View {
+        SwiftUI4ResizableBS()
+    }
+}
